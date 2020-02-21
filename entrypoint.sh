8c8c3454d12c85316d87e21b937a824ba0dd08c7
Rob Moorman <rob@moori.nl>
2020-02-21 08:45:53 +0100

    Use github workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-black --check --diff --line-length 79 /code
-isort --recursive --check-only --diff /code
+black --check --diff --line-length 79 $GITHUB_WORKSPACE
+isort --recursive --check-only --diff $GITHUB_WORKSPACE
