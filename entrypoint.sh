7df389bb54ceef06e826e1bacec88e51a7075813
Rob Moorman <rob@moori.nl>
2020-03-25 13:56:08 +0100

    Fix options

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 cd $GITHUB_WORKSPACE
 
 black --check --diff --line-length 79 .
-isort --recursive --check-only --diff ---force-grid-wrap 0 --trailing-comma --line-width 79 --multi-line 3 --use-parentheses .
+isort --recursive --check-only --diff --force-grid-wrap 0 --trailing-comma --line-width 79 --multi-line 3 --use-parentheses .
