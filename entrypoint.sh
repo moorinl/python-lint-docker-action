563a50ae9e5d240ddd55e514252f378ebc8dc5ef
Rob Moorman <rob@moori.nl>
2020-03-25 14:13:38 +0100

    Remove preferred options

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 
 cd $GITHUB_WORKSPACE
 
-black --check --diff --line-length 79 .
-isort --recursive --check-only --diff --force-grid-wrap 0 --trailing-comma --line-width 79 --multi-line 3 --use-parentheses .
+black --check --diff .
+isort --recursive --check-only --diff .
