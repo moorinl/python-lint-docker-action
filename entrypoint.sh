420af1b68fa6eb549e787070f218f9fcda00528d
Rob Moorman <rob@moori.nl>
2020-03-25 14:22:09 +0100

    Set isort 88

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 cd $GITHUB_WORKSPACE
 
 black --check --diff .
-isort --recursive --check-only --diff --force-grid-wrap 0 --trailing-comma --multi-line 3 --use-parentheses .
+isort --recursive --check-only --diff --force-grid-wrap 0 --trailing-comma --line-width 88 --multi-line 3 --use-parentheses .
