d5f23d9f867179beedae37bb4830b4e71fff5b08
Rob Moorman <rob@moori.nl>
2020-03-25 14:21:49 +0100

    Fix isort compatibility with black defaults

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 cd $GITHUB_WORKSPACE
 
 black --check --diff .
-isort --recursive --check-only --diff .
+isort --recursive --check-only --diff --force-grid-wrap 0 --trailing-comma --multi-line 3 --use-parentheses .
